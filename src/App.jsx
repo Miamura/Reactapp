203add236a61af6eaee55818fc8f8c68dfa6bffb
Miamura <fethiabal@gmail.com>
2024-08-16 11:09:23 +0300

    Hello

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,28 @@
+const Hello = () => {
+  return (
+    <>
+      <p>Hello World</p>
+    </>
+  );
+};
+
 const App = () => {
+  const now = new Date();
+  const a = 10;
+  const b = 20;
+  console.log(now, a + b);
   console.log("Hello from komponetti");
   return (
     <>
       <section>
-        <p>App-komponentissa on sisältö: Hello World!</p>
+        <p>Hello world, it is {now.toString()}</p>
+        <p>
+          {a} plus {b} is {a + b}
+        </p>
+      </section>
+      <section>
+        <h1>Greetings</h1>
+        <Hello />
       </section>
     </>
   );
